1b41a010dfc79ea0398acf31911fd299b8258ec1
ExMaz <173838227+ExMaz@users.noreply.github.com>
2026-02-04 20:58:44 +0000

    Firmware Compiling

---
+++ keyboards/0_keyball/keyball39/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x15, 0xAB, 0xFE, 0x34, 0xD7, 0x74, 0x1F, 0xDB}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define TAP_CODE_DELAY 5
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 1
